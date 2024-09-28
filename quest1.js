30cbf6e20f154fe7de75b5ce07ea6207e2d55a4c
Luis Henrique <luishsilva09@gmail.com>
2024-09-28 14:44:02 -0300

    resposta questão 1

---
+++ quest1.js
@@ -0,0 +1,12 @@
+// Questão 1
+console.log("Questão 1")
+let INDICE = 12
+let SOMA = 0
+let K = 1;
+
+while (K < INDICE) {
+    K = K + 1;
+    SOMA = SOMA + K;
+}
+
+console.log(`Resposta: ${SOMA}`)
